0f659dddf40d52108b8313ab6550c73386f3df4c
Alex Bernsbo <alexkongmee@gmail.com>
2017-10-10 14:18:52 +0200

    certifikat

---
+++ javascript.js
@@ -0,0 +1,91 @@
+// console.log ("Print all number between -10 and 90");
+// var num = -10
+// while (num <= 19){
+//   console.log(num);
+//   num++;
+// }
+//
+// console.log ("Print all even numbers between 10 and 40");
+// var num = 10
+// while (num <= 40)
+// {
+//   console.log(num);
+//   num+=2;
+// }
+//
+// console.log ("Print all odd numbers between 300 and 333");
+// var num = 300;
+// while (num <= 333){
+//   if(num % 2 !== 0){
+//   console.log(num);
+// }
+//   num+=1;
+// }
+//
+// console.log ("Print all numbers divisible by 5 AND 3 beteeen 5 and 50");
+// var num = 5;
+//
+// while (num <= 50){
+//   if(num % 5 === 0 && num % 3 === 0){
+//   console.log(num);
+// }
+//   num+=1;
+// }
+
+// var answer = prompt("Are we there yet?!");
+//
+// while (answer !== "yes" && answer !== "yeah"){
+//   var answer = prompt("Are wer there yet?!");
+// }
+// alert("Yaay, we made it!!!");
+
+// var answer = prompt("Are we there yet?!");
+//
+// while (answer.indexOf ("yes") === -1){
+//   var answer = prompt("Are wer there yet?!");
+// }
+// alert("Yaay, we made it!!!");
+
+// var str = "hello";
+// for (var i = 0; i < str.length; i++) {console.log(str[i]); }
+
+// for(var i = 0; i < 16; i+=8){console.log[i]; }
+
+// var str = "ahceclowlxo";
+// for(var i =1; i < str.length; i+=2) {console.log(str[i]);}
+
+// console.log("Numbers between -10 and 19")
+// for(var i = -10; i < 20; i++) {console.log(i)}
+//
+// console.log ("Even numbers between 10 and 40");
+// for(var i = 10; i <= 40; i+=1){
+//   if (i % 2 === 0){
+//     console.log(i);
+//   }
+// }
+//
+// console.log ("Odd numbers between 300 and 333");
+// for (var i = 300; i <= 333; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+//
+// console.log ("Print all numbers divisible by 5 AND 3 beteeen 5 and 50");
+// for (var i = 5; i <= 50; i++) {
+//   if (i % 5 === 0 && i % 3 === 0 ){
+//     console.log(i);
+//   }
+// }
+
+// function isEven(num){
+//   return num % 2 === 0;
+// }
+
+function factorial(num){
+var result = 1;
+for (var i = 2; i <= num; i++){
+  result *= i;
+}
+return result;
+}
